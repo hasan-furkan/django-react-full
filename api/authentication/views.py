3c3ccfd32497951fa2fa6bbf7e59377c12f56af9
hasan-furkan <hsnfrkn32@gmail.com>
2023-04-10 15:38:24 +0300

    kullanici kaydindan sonra email gonderme aktif edildi

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -3,13 +3,35 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from .serializers import UserSerializer
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+
+def send_verification_email(user_email, user_name):
+    subject = 'Please verify your account'
+    from_email = 'hsnfrkn32@gmail.com'
+    to_email = user_email
+    context = {
+        'product': 'HFKSHOP',
+        'name': user_name,
+    }
+    html_message = render_to_string('welcome.html', context)
+    plain_message = strip_tags(html_message)
+    email = EmailMessage(
+        subject=subject,
+        body=plain_message,
+        from_email=from_email,
+        to=[to_email],
+    )
+    email.send()
 
 
 class RegisterView(generics.CreateAPIView, generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get_queryset(self):
+    def get_queryset(self, request, *args, **kwargs):
         return User.objects.all()
 
     def post(self, request, *args, **kwargs):
@@ -23,5 +45,6 @@ class RegisterView(generics.CreateAPIView, generics.ListAPIView):
         user = User(email=email, password=make_password(password))
         user.save()
         serializer = UserSerializer(user)
-        print(serializer.data)
+        send_verification_email(request.data.get('email'), request.data.get('name'))
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
