8d44dc06b609dc7dc443065b2da0131628c64eb5
hasan-furkan <hsnfrkn32@gmail.com>
2023-04-11 11:18:45 +0300

    loginapiview eklendi

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate, login
 from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from .serializers import UserSerializer
@@ -25,12 +27,7 @@ def send_verification_email(user_email, user_name):
     email.send()
 
 
-class RegisterView(generics.CreateAPIView, generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return User.objects.all()
+class RegisterAPIView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         email = request.data.get('email')
@@ -49,4 +46,30 @@ class RegisterView(generics.CreateAPIView, generics.ListAPIView):
         serializer = UserSerializer(user)
         send_verification_email(request.data.get('email'), request.data.get('first_name'))
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"status": True, "message": serializer.data}, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login_count = User.objects.get(login_count=request.data.get('login_count'))
+        print(login_count)
+
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is None:
+            login_count += 1
+            return Response({"status": False, 'message': 'password or username is wrong'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        elif login_count > 6:
+            return Response({'status': False, 'message': 'You have exceeded the number of login attempts.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            login(request, user)
+            return Response({'message': 'Kullanıcı oturum açtı.'})
--- api/user/admin.py
+++ api/user/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import UserModals
 # Register your models here.
+
+admin.site.register(UserModals)
