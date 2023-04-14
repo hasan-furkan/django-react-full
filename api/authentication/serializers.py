688b62db63bf39e371331f1344c415ddbd329584
hasan-furkan <hsnfrkn32@gmail.com>
2023-04-14 16:02:58 +0300

    login view serializer duzenlendi

--- api/authentication/serializers.py
+++ api/authentication/serializers.py
@@ -1,10 +1,39 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers, status
 from user.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(read_only=True)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        validated_data['password'] = make_password(password)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password')
+        validated_data['password'] = make_password(password)
+        return super().update(instance, validated_data)
 
     class Meta:
         model = User
         fields = ('full_name', 'email', 'password')
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        if email and password:
+            user = authenticate(request=self.context.get('request'), email=email, password=password)
+            if not user:
+                raise serializers.ValidationError('Invalid credentials')
+        else:
+            raise serializers.ValidationError('Email and password are required')
+        attrs['user'] = user
+        return attrs
--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, UserLoginView
 
 urlpatterns = [
     path('register', UserRegisterView.as_view(), name='register'),
-    # path('login', UserLoginView.as_view(), name='login'),
+    path('login', UserLoginView.as_view(), name='login'),
 ]
