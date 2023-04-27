8196888357957479a7bd2a230dbd865990c868f9
hasan-furkan <hsnfrkn32@gmail.com>
2023-04-27 11:02:01 +0300

    user verify eklendi

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -4,18 +4,20 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.hashers import make_password, check_password
 
-from .serializers import RegisterSerializer, LoginSerializer
+from utils.util import get_uuid0
+from .serializers import RegisterSerializer, LoginSerializer, UserVerifySerializer
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
 from user.models import User
 
 
-def send_verification_email(user_email, user_name, template_name):
+def send_verification_email(user_email, user_name, token, template_name):
     to_email = user_email
     context = {
         'product': 'HFKSHOP',
         'name': user_name,
+        'token': token,
     }
     html_message = render_to_string(template_name, context)
     email = EmailMessage(
@@ -34,9 +36,9 @@ class UserRegisterView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
+            User.token = get_uuid0()
             serializer.save()
-            # send_verification_email(request.data.get('email'), request.data.get('fullName'), 'confirm-email.html')
-
+            send_verification_email(request.data.get('email'), request.data.get('fullName'), 'confirm-email.html')
             return Response({
                 "status": True,
                 "message": serializer.data
@@ -45,6 +47,32 @@ class UserRegisterView(APIView):
             "status": False,
             "message": serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserVerifyView(APIView):
+    serializer_class = UserVerifySerializer
+
+    def get(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = request.data.get('email')
+            user = User.objects.filter(email=email).first()
+            if user:
+                user.is_verified = True
+                user.save()
+                return Response({
+                    "status": True,
+                    "message": serializer.data
+                }, status=status.HTTP_200_OK)
+            return Response({
+                "status": False,
+                "message": "User not found"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        return Response({
+            "status": False,
+            "message": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class UserLoginView(APIView):
---
+++ api/utils/util.py
@@ -0,0 +1,5 @@
+import uuid0
+
+
+def get_uuid0():
+    return uuid0.generate()
